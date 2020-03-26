6c64c3356cd476a82a022c3f7594f4b8cb804830
itdev-lenovo <it1devops@gmail.com>
2020-03-26 20:07:02 +0530

    files added

---
+++ Levle1/Controllers/NewController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Levle1.Controllers
+{
+    public class NewController : Controller
+    {
+        public IActionResult Index()
+        {
+            //Add Nuget packages and migrations and add docker support from Visual Studio
+
+
+
+
+
+
+
+
+
+
+
+            return View();
+        }
+    }
+}
